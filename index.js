e24445bb9fff23c1c790e335e2b543b5baf72b1f
young <liyang_email@163.com>
2024-06-23 21:14:47 +0800

    pref: modify path to md5

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const path = require('path');
+const crypto = require('crypto');
+const formatPath = require("./lib/formatPath")
 const createIssues = require('./lib/createIssues');
 
 hexo.extend.filter.register('new_post_path', async function (data) {
     const postName = path.basename(data, path.extname(data)); // withdraw post name
-    await createIssues(hexo.config, postName) // create github issues
+    const formatGitalkPath = formatPath(postName); // format gitalk path_md5
+    const path_md5 = crypto.createHash('md5').update(formatGitalkPath).digest('hex')
+    await createIssues(hexo.config, postName, path_md5) // create github issues
     return data;
 });
